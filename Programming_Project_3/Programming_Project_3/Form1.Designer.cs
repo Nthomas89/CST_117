5cf21029a0941024ffe4f740d8cf1227e48dff30
Nicholas Thomas <Nthomas39@my.gcu.edu>
2018-03-25 23:08:05 -0700

    Programming_Project_3

---
+++ Programming_Project_3/Programming_Project_3/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Programming_Project_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_OpenFile = new System.Windows.Forms.Button();
+            this.listBox1_Results = new System.Windows.Forms.ListBox();
+            this.button1_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 93);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Open a text file\r\n    to Analyze\r\n   Characters";
+            // 
+            // button1_OpenFile
+            // 
+            this.button1_OpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_OpenFile.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1_OpenFile.Location = new System.Drawing.Point(134, 163);
+            this.button1_OpenFile.Name = "button1_OpenFile";
+            this.button1_OpenFile.Size = new System.Drawing.Size(117, 53);
+            this.button1_OpenFile.TabIndex = 1;
+            this.button1_OpenFile.Text = "Open File";
+            this.button1_OpenFile.UseVisualStyleBackColor = true;
+            this.button1_OpenFile.Click += new System.EventHandler(this.button1_OpenFile_Click);
+            // 
+            // listBox1_Results
+            // 
+            this.listBox1_Results.FormattingEnabled = true;
+            this.listBox1_Results.Location = new System.Drawing.Point(33, 258);
+            this.listBox1_Results.Name = "listBox1_Results";
+            this.listBox1_Results.Size = new System.Drawing.Size(325, 95);
+            this.listBox1_Results.TabIndex = 2;
+            // 
+            // button1_Close
+            // 
+            this.button1_Close.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_Close.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1_Close.Location = new System.Drawing.Point(134, 401);
+            this.button1_Close.Name = "button1_Close";
+            this.button1_Close.Size = new System.Drawing.Size(117, 37);
+            this.button1_Close.TabIndex = 3;
+            this.button1_Close.Text = "Close";
+            this.button1_Close.UseVisualStyleBackColor = true;
+            this.button1_Close.Click += new System.EventHandler(this.button1_Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.button1_Close);
+            this.Controls.Add(this.listBox1_Results);
+            this.Controls.Add(this.button1_OpenFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Programming Project 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1_OpenFile;
+        private System.Windows.Forms.ListBox listBox1_Results;
+        private System.Windows.Forms.Button button1_Close;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Programming_Project_3/Programming_Project_3/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Project_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_OpenFile_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Declare variables
+                String readText;
+                String[] currentWord;
+                String firstWordAlphabetically = "";
+                String lastWordAlphabetically = "";
+                String longestWord = "";
+                String mostVowels = "";
+                char[] wordCharArray;
+                int currentVowelCount;
+                int mostVowelCount = 0;
+
+                //Clear the list box
+                listBox1_Results.Items.Clear();
+
+                try
+                {
+                    //Setup for file system.
+                    StreamReader textFile = new StreamReader(openFileDialog1.FileName);
+                    SaveFileDialog savefile = new SaveFileDialog();
+                    StreamWriter saveFile = File.CreateText("Results_Programming_Project_3.txt");
+
+                    while (!textFile.EndOfStream)
+                    {
+                        //load file
+                        readText = textFile.ReadLine();
+                        // lowe case text
+                        readText = readText.ToLower();
+                        //Split the line if more than one word
+                        currentWord = readText.Split(' ');
+
+                        //loop current word within the array
+                        for (int i = 0; i < currentWord.Length; i++)
+                        {
+
+                            currentVowelCount = 0;
+                            //Character array for searching for vowels.
+                            wordCharArray = currentWord[i].ToCharArray();
+
+                            //first word
+                            if (firstWordAlphabetically == "")
+                            {
+                               
+                                firstWordAlphabetically = currentWord[i];
+                            }
+                            else if (currentWord[i].CompareTo(firstWordAlphabetically) < 0)
+                            {
+                              
+                                firstWordAlphabetically = currentWord[i];
+                            }
+                            //last word
+                            if (lastWordAlphabetically == "")
+                            {
+                                lastWordAlphabetically = currentWord[i];
+                            }
+                            else if (currentWord[i].CompareTo(lastWordAlphabetically) > 0)
+                            {
+                                lastWordAlphabetically = currentWord[i];
+                            }
+
+                            //longest word
+                            if (longestWord == "")
+                            {
+                                longestWord = currentWord[i];
+                            }
+                            else if (longestWord.Length < currentWord[i].Length)
+                            {
+                                longestWord = currentWord[i];
+                            }
+
+                            //find vowels
+                            for (int j = 0; j < wordCharArray.Length; j++)
+                            {
+                                if (wordCharArray[j] == 'a' ||
+                                wordCharArray[j] == 'e' ||
+                                wordCharArray[j] == 'i' ||
+                                wordCharArray[j] == 'o' ||
+                                wordCharArray[j] == 'u')
+                                {
+                                    currentVowelCount++;
+                                }
+                            }
+
+                            //Compare vowels.
+                            if (currentVowelCount > mostVowelCount)
+                            {
+                                mostVowelCount = currentVowelCount;
+                                mostVowels = currentWord[i];
+                            }
+                        }
+                    }
+
+                    //Write list box.
+                    listBox1_Results.Items.Add("First word alphabetically: " + firstWordAlphabetically);
+                    listBox1_Results.Items.Add("Last word alphabetically: " + lastWordAlphabetically);
+                    listBox1_Results.Items.Add("Longest word: " + longestWord + " at " + longestWord.Length + " characters.");
+                    listBox1_Results.Items.Add("Word with the most vowels: " + mostVowels + " with " + mostVowelCount + " vowels");
+
+                    //save the file
+                    saveFile.WriteLine("First word alphabetically: " + firstWordAlphabetically);
+                    saveFile.WriteLine("Last word alphabetically: " + lastWordAlphabetically);
+                    saveFile.WriteLine("Longest word: " + longestWord + " at " + longestWord.Length + " characters.");
+                    saveFile.WriteLine("Word with the most vowels: " + mostVowels + " with " + mostVowelCount + " vowels");
+
+                    saveFile.Close();
+                    textFile.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void button1_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
