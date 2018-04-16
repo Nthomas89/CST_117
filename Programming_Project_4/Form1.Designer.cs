b7caf265ba8332cc506a15e733a6dfe701e55c41
Nicholas Thomas <Nthomas39@my.gcu.edu>
2018-04-15 22:34:24 -0700

    PRogramming_Project_4

---
+++ Programming_Project_4/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Programming_Project_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button_NEWGAME = new System.Windows.Forms.Button();
+            this.text_Winner = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(110, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(181, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // button_NEWGAME
+            // 
+            this.button_NEWGAME.Location = new System.Drawing.Point(44, 201);
+            this.button_NEWGAME.Name = "button_NEWGAME";
+            this.button_NEWGAME.Size = new System.Drawing.Size(172, 23);
+            this.button_NEWGAME.TabIndex = 9;
+            this.button_NEWGAME.Text = "NEW GAME";
+            this.button_NEWGAME.UseVisualStyleBackColor = true;
+            this.button_NEWGAME.Click += new System.EventHandler(this.button_NEWGAME_Click);
+            // 
+            // text_Winner
+            // 
+            this.text_Winner.Location = new System.Drawing.Point(44, 3);
+            this.text_Winner.Name = "text_Winner";
+            this.text_Winner.Size = new System.Drawing.Size(172, 20);
+            this.text_Winner.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 450);
+            this.Controls.Add(this.text_Winner);
+            this.Controls.Add(this.button_NEWGAME);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button_NEWGAME;
+        private System.Windows.Forms.TextBox text_Winner;
+    }
+}
+
---
+++ Programming_Project_4/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Project_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_NEWGAME_Click(object sender, EventArgs e)
+        {
+            // Initilize random number object
+            Random rand = new Random();
+
+            // size of board
+            const int rows = 3;
+            const int columns = 3;
+
+            // two-dimensional int array.
+            int[,] playingBoard = new int[rows, columns];
+
+            // Fill array with random integers between 0 and 1
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    playingBoard[row, col] = rand.Next(2);
+                }
+            }
+
+            // values from array
+            label1.Text = changeValue(playingBoard[0, 0]);
+            label2.Text = changeValue(playingBoard[0, 1]);
+            label3.Text = changeValue(playingBoard[0, 2]);
+
+            label4.Text = changeValue(playingBoard[1, 0]);
+            label5.Text = changeValue(playingBoard[1, 1]);
+            label6.Text = changeValue(playingBoard[1, 2]);
+
+            label7.Text = changeValue(playingBoard[2, 0]);
+            label8.Text = changeValue(playingBoard[2, 1]);
+            label9.Text = changeValue(playingBoard[2, 2]);
+
+            //O win check
+            if (playingBoard[0, 0] == 0 && (playingBoard[0, 0] == playingBoard[0, 1]) && (playingBoard[0, 1] == playingBoard[0, 2]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[1, 0] == 0 && (playingBoard[1, 0] == playingBoard[1, 1]) && (playingBoard[1, 1] == playingBoard[1, 2]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[2, 0] == 0 && (playingBoard[2, 0] == playingBoard[2, 1]) && (playingBoard[2, 1] == playingBoard[2, 2]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[0, 0] == 0 && (playingBoard[0, 0] == playingBoard[1, 0]) && (playingBoard[1, 0] == playingBoard[2, 0]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[0, 1] == 0 && (playingBoard[0, 1] == playingBoard[1, 1]) && (playingBoard[1, 1] == playingBoard[2, 1]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[0, 2] == 0 && (playingBoard[0, 2] == playingBoard[1, 2]) && (playingBoard[1, 2] == playingBoard[2, 2]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[0, 0] == 0 && (playingBoard[0, 0] == playingBoard[1, 1]) && (playingBoard[1, 1] == playingBoard[2, 2]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+            else if (playingBoard[0, 2] == 0 && (playingBoard[0, 2] == playingBoard[1, 1]) && (playingBoard[0, 1] == playingBoard[2, 0]))
+            {
+                text_Winner.Text = "Winner is O!";
+            }
+
+
+            //X win check
+            if (playingBoard[0, 0] == 1 && (playingBoard[0, 0] == playingBoard[0, 1]) && (playingBoard[0, 1] == playingBoard[0, 2]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[1, 0] == 1 && (playingBoard[1, 0] == playingBoard[1, 1]) && (playingBoard[1, 1] == playingBoard[1, 2]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[2, 0] == 1 && (playingBoard[2, 0] == playingBoard[2, 1]) && (playingBoard[2, 1] == playingBoard[2, 2]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[0, 0] == 1 && (playingBoard[0, 0] == playingBoard[1, 0]) && (playingBoard[1, 0] == playingBoard[2, 0]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[0, 1] == 1 && (playingBoard[0, 1] == playingBoard[1, 1]) && (playingBoard[1, 1] == playingBoard[2, 1]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[0, 2] == 1 && (playingBoard[0, 2] == playingBoard[1, 2]) && (playingBoard[1, 2] == playingBoard[2, 2]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[0, 0] == 1 && (playingBoard[0, 0] == playingBoard[1, 1]) && (playingBoard[1, 1] == playingBoard[2, 2]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }
+            else if (playingBoard[0, 2] == 1 && (playingBoard[0, 2] == playingBoard[1, 1]) && (playingBoard[0, 1] == playingBoard[2, 0]))
+            {
+                text_Winner.Text = "Winner is X!";
+            }  
+        }
+
+        // change integers to string value
+        public string changeValue(int integerValue)
+        {
+            if (integerValue == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+    }
+}
+ 
