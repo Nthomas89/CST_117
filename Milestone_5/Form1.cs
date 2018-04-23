39fcee507b60bc779c370630d08c5797a003457d
Nicholas Thomas <Nthomas39@my.gcu.edu>
2018-04-22 23:36:26 -0700

    Milestone_5
    updated look and feel.

---
+++ Milestone_5/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone_1
+{
+    public partial class Form1 : Form
+    {
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void converter_Picture_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            var newwindow = new converter();
+
+            newwindow.Show();
+        }
+
+        private void transmission_Picture_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            var newwindow = new transmission();
+
+            newwindow.Show();
+        }
+
+        // close
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Inventory_Click(object sender, EventArgs e)
+        {
+            Inventory_Manager partsList = new Inventory_Manager();
+
+            // add items
+            //partsList.addItem(item1);
+            //partsList.addItem(item2);
+            //partsList.addItem(item3);
+
+            // display updated list
+            //Console.WriteLine("!Updated Inventory List!");
+            //partsList.displayInventory();
+
+            // remove items 
+            //partsList.removeItem(item2);
+            //partsList.removeItem(item3);
+
+            // display updated list
+            Console.WriteLine(" ");
+            Console.WriteLine("!Updated Inventory List!");
+            partsList.displayInventory();
+
+            // restock items 
+           // partsList.restockItem(item, 20);
+
+            // display updated list
+            Console.WriteLine(" ");
+            Console.WriteLine("!Updated Inventory List!");
+            partsList.displayInventory();
+        }
+    }
+ }
---
+++ Milestone_5/transmission.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Milestone_1
+{
+    partial class transmission
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_BKT = new System.Windows.Forms.Button();
+            this.button_AddT = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_NameT = new System.Windows.Forms.TextBox();
+            this.textBox_NumberT = new System.Windows.Forms.TextBox();
+            this.textBox_QuantityT = new System.Windows.Forms.TextBox();
+            this.Display = new System.Windows.Forms.ListBox();
+            this.button_Inventory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_BKT
+            // 
+            this.button_BKT.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button_BKT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_BKT.ForeColor = System.Drawing.Color.Indigo;
+            this.button_BKT.Location = new System.Drawing.Point(266, 343);
+            this.button_BKT.Name = "button_BKT";
+            this.button_BKT.Size = new System.Drawing.Size(103, 49);
+            this.button_BKT.TabIndex = 0;
+            this.button_BKT.Text = "BACK";
+            this.button_BKT.UseVisualStyleBackColor = false;
+            this.button_BKT.Click += new System.EventHandler(this.button_BKT_Click);
+            // 
+            // button_AddT
+            // 
+            this.button_AddT.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button_AddT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_AddT.ForeColor = System.Drawing.Color.Indigo;
+            this.button_AddT.Location = new System.Drawing.Point(35, 343);
+            this.button_AddT.Name = "button_AddT";
+            this.button_AddT.Size = new System.Drawing.Size(103, 49);
+            this.button_AddT.TabIndex = 1;
+            this.button_AddT.Text = "ADD";
+            this.button_AddT.UseVisualStyleBackColor = false;
+            this.button_AddT.Click += new System.EventHandler(this.button_AddT_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(89, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 59);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Transmission";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(49, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Part Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(30, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Part Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(24, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Item Quantity";
+            // 
+            // textBox_NameT
+            // 
+            this.textBox_NameT.Location = new System.Drawing.Point(228, 75);
+            this.textBox_NameT.Name = "textBox_NameT";
+            this.textBox_NameT.Size = new System.Drawing.Size(153, 20);
+            this.textBox_NameT.TabIndex = 6;
+            // 
+            // textBox_NumberT
+            // 
+            this.textBox_NumberT.Location = new System.Drawing.Point(228, 137);
+            this.textBox_NumberT.Name = "textBox_NumberT";
+            this.textBox_NumberT.Size = new System.Drawing.Size(153, 20);
+            this.textBox_NumberT.TabIndex = 7;
+            // 
+            // textBox_QuantityT
+            // 
+            this.textBox_QuantityT.Location = new System.Drawing.Point(228, 191);
+            this.textBox_QuantityT.Name = "textBox_QuantityT";
+            this.textBox_QuantityT.Size = new System.Drawing.Size(153, 20);
+            this.textBox_QuantityT.TabIndex = 8;
+            // 
+            // Display
+            // 
+            this.Display.FormattingEnabled = true;
+            this.Display.Location = new System.Drawing.Point(35, 233);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(352, 95);
+            this.Display.TabIndex = 9;
+            this.Display.SelectedIndexChanged += new System.EventHandler(this.Display_SelectedIndexChanged);
+            // 
+            // button_Inventory
+            // 
+            this.button_Inventory.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button_Inventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Inventory.ForeColor = System.Drawing.Color.Indigo;
+            this.button_Inventory.Location = new System.Drawing.Point(144, 343);
+            this.button_Inventory.Name = "button_Inventory";
+            this.button_Inventory.Size = new System.Drawing.Size(116, 49);
+            this.button_Inventory.TabIndex = 10;
+            this.button_Inventory.Text = "INVENTORY";
+            this.button_Inventory.UseVisualStyleBackColor = false;
+            this.button_Inventory.Click += new System.EventHandler(this.button_Inventory_Click);
+            // 
+            // transmission
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(426, 450);
+            this.Controls.Add(this.button_Inventory);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.textBox_QuantityT);
+            this.Controls.Add(this.textBox_NumberT);
+            this.Controls.Add(this.textBox_NameT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_AddT);
+            this.Controls.Add(this.button_BKT);
+            this.Name = "transmission";
+            this.Text = "transmission";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_BKT;
+        private System.Windows.Forms.Button button_AddT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_NameT;
+        private System.Windows.Forms.TextBox textBox_NumberT;
+        private System.Windows.Forms.TextBox textBox_QuantityT;
+        private System.Windows.Forms.ListBox Display;
+        private System.Windows.Forms.Button button_Inventory;
+    }
+}
